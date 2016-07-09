52029ec64282487ac7e24feadb842c1ddfbf70ce
Matt <matthewsharp123@gmail.com>
2016-07-09 12:08:39 +0100

    add diamond square functions that modify correct array entries

--- diamond-square-heightmap.js
+++ diamond-square-heightmap.js
@@ -1,5 +1,5 @@
 
-const size = 100;
+const magnitude = 100;
 const detail = 2;
 const roughness = 1 // 0-1
 
@@ -23,21 +23,45 @@ function setInitialConditions(arr) {
 function diamond(arr, size) {
   const array = arr;
   const half = size / 2;
-  const max = Math.sqrt(array.length) - 1
-  for (let y = half; y < max; y += size) {
-    for (let x = half; x < max; x += size) {
-
+  const max = Math.sqrt(array.length);
+  for (let y = 0; y < max - 1; y += size) {
+    for (let x = 0; x < max - 1; x += size) {
+      array[x + half + ((y + half) * (max))] = 1;
+      console.log(x, y)
     }
   }
   return array;
 }
 
 function square(arr, size) {
-
+  const array = arr;
+  const half = size / 2;
+  const max = Math.sqrt(array.length);
+  for (let y = 0; y < max - 1; y += size) {
+    for (let x = 0; x < max - 1; x += size) {
+      array[x + half + (y * max)] = 1; // Top middle
+      // console.log(x + half + (y * max))
+      array[x + ((y + half) * (max))] = 1; // Left middle
+      // console.log(x + ((y + half) * max))
+      array[x + size + ((y + half) * (max))] = 1; // Right middle
+      // console.log(x + max + ((y + half) * max))
+      array[x + half + ((y + size) * (max))] = 1; // Bottom middle
+      // console.log(x + half + ((y + (size - 1)) * max))
+      // array[] = 1
+      // array[(x) + (half * y * max)] = 1;
+    }
+  }
+  return array;
 }
 
 let terrain = createTerrain(detail);
 let initialHeightMap = setInitialConditions(terrain);
-//let diamond1 = diamond(initialHeightMap, Math.sqrt(initialHeightMap.length) - 1)
-
-console.log(initialHeightMap)
+// console.log(initialHeightMap)
+let diamond1 = diamond(initialHeightMap, 4)
+console.log(diamond1)
+let square1 = square(diamond1, 4)
+console.log(square1)
+let diamond2 = diamond(square1, 2)
+console.log(diamond2)
+let square2 = square(diamond2, 2)
+console.log(square2)
